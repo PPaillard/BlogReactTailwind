c98993193fae94562ee59148e719db273e52714d
PierreP <53476143+RoYaLSleT@users.noreply.github.com>
2022-11-28 20:06:55 +0100

    Added test vh conf

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="sticky text-font-one h-20 top-0 backdrop-blur-md z-30">
+      Header
+    </div>
+  );
 };
 
 export default Header;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return <div className="h-120v">Home</div>;
 };
 
 export default Home;
