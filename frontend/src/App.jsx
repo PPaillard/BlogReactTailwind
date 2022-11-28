dd31586caac419ceee314a9db627fd9c9e129170
PierreP <53476143+RoYaLSleT@users.noreply.github.com>
2022-11-28 19:53:25 +0100

    added tailwind support and base components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
+import Header from "./components/Header";
 import Home from "./pages/Home";
-
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Header />
       <Home />
-      <p>coucou</p>
-    </div>
+      <Footer />
+    </>
   );
 }
 
